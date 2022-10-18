c9cde38f1597cf3d5383ecc76bdd41c571d66660
Andy Park <seminpar@usc.edu>
2022-10-18 19:02:57 +0000

    Added a couple of missing files

---
+++ stack.h
@@ -0,0 +1,66 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <vector>
+#include <stdexcept>
+
+// Use inheritance from std::vector (choose public/private) as appropriate
+template <typename T>
+class Stack : private std::vector<T>
+{
+public:
+    Stack();
+    //~Stack();
+    bool empty() const;
+    size_t size() const;
+    void push(const T& item);
+    void pop();  // throws std::underflow_error if empty
+    const T& top() const; // throws std::underflow_error if empty
+    // Add other members only if necessary
+};
+
+template<typename T>
+Stack<T>::Stack() : std::vector<T>()
+{}
+
+// template <typename T>
+// Stack<T>::~Stack(){
+// }
+
+template <typename T>
+bool Stack<T>::empty() const {
+    if (std::vector<T>::size() == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+size_t Stack<T>::size() const {
+    return std::vector<T>::size();
+}
+
+template <typename T>
+void Stack<T>::push(const T& item){
+    this->push_back(item);
+}
+
+template <typename T>
+void Stack<T>::pop(){
+    if (this->empty()){
+        throw std::underflow_error("Stack is empty!");
+    }
+    this->pop_back();
+}
+
+template <typename T>
+const T& Stack<T>::top() const{
+    if (this->empty()){
+        throw std::underflow_error("Stack is empty!");
+    }
+    return this->back();
+}
+
+
+#endif
